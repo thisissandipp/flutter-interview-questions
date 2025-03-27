e61e63856c7fab71d8034c1bf1fd7289bb1aeb6c
Sandip <sandipramanik07@gmail.com>
2025-03-28 01:47:46 +0530

    set up a dart project

---
+++ bin/flutter_interview_questions.dart
@@ -0,0 +1,5 @@
+import 'package:flutter_interview_questions/flutter_interview_questions.dart' as flutter_interview_questions;
+
+void main(List<String> arguments) {
+  print('Hello world: ${flutter_interview_questions.calculate()}!');
+}
